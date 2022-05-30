c6a45aa4c50d306aa3759a7e95dd31eaa1ce66ca
victor DG <v.degermain@gmail.com>
2022-05-30 11:55:18 +0200

    ajout pros

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Fragment } from "react/cjs/react.production.min";
 
-const name="toto";
-const tel="toto";
-const email="toto@gmail.com";
+// const name="toto";
+// const tel="toto";
+// const email="toto@gmail.com";
 
-const  Contact = ()=>{
+const  Contact = (props)=>{
+    console.log(`${props.name} ${props.tel} ${props.email}`)
    return(
         <Fragment>
-            <h3>{name}</h3>
-            <h4>{tel}</h4>
-            <h4>{email}</h4>
+            <h3>{props.name}</h3>
+            <h4>{props.tel}</h4>
+            <h4>{props.email}</h4>
             <button>Suppresion</button>
         </Fragment>
    )
-    
 }
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ export const App = (
   <div>
     <h1 className="text-center">My contacts list 📱</h1>
       <div>
-         <Contact />
-         <Contact />
-         <Contact />
-         <Contact />
-         <Contact />
+         <Contact name="JEan" tel="0606062206" email ="jean.d@gmail.com"/>
+         <Contact name="Marc" tel="060160606" email ="sdd.d@gmail.com"/>
+         <Contact name="toto" tel="0602260606" email ="ff.d@gmail.com"/>
+         <Contact name="lucie" tel="06060606" email ="fe.d@gmail.com"/>
+         <Contact name="lena" tel="060260606" email ="ee.d@gmail.com"/>
       </div>
   </div>
 );
